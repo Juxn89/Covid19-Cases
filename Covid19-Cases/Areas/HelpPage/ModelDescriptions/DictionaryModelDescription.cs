bfced08168edf61f94ca167f6149b7f999107e19
jgomez_89 <juan.gomez@axxis-systems.com>
2021-10-02 13:00:57 -0600

    Agregar archivos de proyecto.

---
+++ Covid19-Cases/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Covid19_Cases.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Covid19-Cases/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Covid19_Cases.WebApiApplication" Language="C#" %>
